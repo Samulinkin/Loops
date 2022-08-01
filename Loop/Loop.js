d461da3e5e9c8f5905ce6b35d4b8e2fabfcd924a
Samuel Rodrigues <samulinkin@gmail.com>
2022-08-01 12:07:29 -0300

    Loops2

--- Loop/Loop.js
+++ Loop/Loop.js
@@ -1,4 +1,10 @@
-for (let i = 0; i < 10; i++) {
-    console.log(i)
-   
+const funcs = []
+for(let i = 0; i < 10; i ++){ // Pelo fato de ter escopo de bloco o valor de 'i' será lembrada 
+    funcs.push(function(){
+        console.log(i)
+    })
 }
+funcs [2] ()
+funcs [6] ()
+funcs [8] ()
+ 
